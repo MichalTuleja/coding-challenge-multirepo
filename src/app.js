42ce44861726f2d92a923610d6e06bbda497b4f8
Michal Tuleja <michal.r.tuleja@gmail.com>
2023-01-23 17:16:49 +0100

    Fix get contract API

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { sequelize } = require('./model');
 const { getProfile } = require('./middleware/getProfile');
+const httpStatus = require('http-status');
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize);
@@ -14,8 +15,26 @@ app.set('models', sequelize.models);
 app.get('/contracts/:id', getProfile, async (req, res) => {
     const { Contract } = req.app.get('models');
     const { id } = req.params;
-    const contract = await Contract.findOne({ where: { id } });
-    if (!contract) return res.status(404).end();
+    const { id: profile_id, type } = req.profile.dataValues;
+
+    let contract;
+
+    console.log(profile_id);
+    console.log(type);
+
+    switch (type) {
+        case 'client':
+            contract = await Contract.findOne({ where: { id, 'ClientId': profile_id } });
+            break;
+        case 'contractor':
+            contract = await Contract.findOne({ where: { id, 'ContractorId': profile_id } });
+            break;
+        default:
+            return res.status(httpStatus.BAD_REQUEST).end();
+    }
+
+    if (!contract) return res.status(httpStatus.NOT_FOUND).end();
     res.json(contract);
 });
+
 module.exports = app;
