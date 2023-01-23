683a5d6ef4b9984ccc194d274de92de08a2adbe1
Michal Tuleja <michal.r.tuleja@gmail.com>
2023-01-23 17:22:14 +0100

    Update getProfile middleware

--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,10 +1,18 @@
+/*
+ * For debugging purpose: Keep the the ID above 0 if not provided
+ * That constant simulates the case when a user is authenticated
+ * and has access to resources related them.
+ */
+const FALLBACK_PROFILE_ID = 5;
+
 const getProfile = async (req, res, next) => {
     const { Profile } = req.app.get('models');
     const profile = await Profile.findOne({
-        where: { id: req.get('profile_id') || 0 },
+        where: { id: req.get('profile_id') || FALLBACK_PROFILE_ID },
     });
     if (!profile) return res.status(401).end();
     req.profile = profile;
     next();
 };
+
 module.exports = { getProfile };
