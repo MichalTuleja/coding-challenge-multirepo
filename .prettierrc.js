8e189eca5da4c5bc790901ee451eb5e51c980db4
Michal Tuleja <michal.r.tuleja@gmail.com>
2023-01-23 16:54:02 +0100

    Add prettier to dependencies

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"bracketSpacing": true,
+	"jsxBracketSameLine": true,
+	"singleQuote": true,
+	"trailingComma": "all",
+	"arrowParens": "avoid",
+	"tabWidth": 4,
+	"useTabs": false,
+	"printWidth": 80,
+	"singleAttributePerLine": true,
+};
