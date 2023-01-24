169d82e02e56a604cdc7a3e2d7075aa83ea88253
Michal Tuleja <michal.r.tuleja@gmail.com>
2023-01-24 03:44:07 +0100

    Add unpaid jobs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { sequelize } = require('./model');
+const { sequelize, Contract } = require('./model');
+const { Op, ExclusionConstraintError } = require("sequelize");
 const { getProfile } = require('./middleware/getProfile');
 const httpStatus = require('http-status');
 const app = express();
@@ -11,6 +12,16 @@ const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
+
+
+/*
+ * Helpers
+ */
+const typeToIdDict = {
+    'client': 'ClientId',
+    'contractor': 'ContractorId',
+};
+
 
 /*
  * Controllers
@@ -73,6 +84,41 @@ const getContracts = async (req, res) => {
     res.json(contract);
 };
 
+const getUnpaidJobs = async (req, res) => {
+    const PAGINATION_LIMIT = 2;
+
+    const page = req.params.page || 0;
+
+    const { Job, Contract } = req.app.get('models');
+    const { id: profile_id, type } = req.profile.dataValues;
+
+    let jobs;
+
+    jobs = await Job.findAndCountAll({
+        limit: PAGINATION_LIMIT,
+        offset: PAGINATION_LIMIT * page,
+        where: { 
+            'paid': { [Op.not]: true } 
+        },
+        include: [
+            { 
+                model: Contract, 
+                where: {
+                    [typeToIdDict[type]]: profile_id,
+                    [Op.or]: [
+                        { 'status': 'in_progress' },
+                        { 'status': 'new' },
+                    ],
+                },
+                required: true,
+            },
+        ],
+    });
+
+    if (!jobs) return res.status(httpStatus.NOT_FOUND).end();
+    res.json(jobs);
+};
+
 const notImplemented = async (req, res) => {
     return res.status(httpStatus.NOT_IMPLEMENTED).end();
 };
@@ -82,7 +128,7 @@ const notImplemented = async (req, res) => {
  */
 app.get('/contracts/:id', getProfile, getContractById);
 app.get('/contracts', getProfile, getContracts);
-app.get('/jobs/unpaid', getProfile, notImplemented);
+app.get('/jobs/unpaid', getProfile, getUnpaidJobs);
 app.post('/jobs/:job_id/pay', getProfile, notImplemented);
 app.post('/balances/deposit/:userId', getProfile, notImplemented);
 app.post('/admin/best-profession?start=<date>&end=<date>', getProfile, notImplemented);
