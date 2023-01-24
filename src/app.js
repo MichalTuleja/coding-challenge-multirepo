11bf1603fafd524c76bdb7acbab3acfb56955135
Michal Tuleja <michal.r.tuleja@gmail.com>
2023-01-24 04:27:34 +0100

    Update pay for job

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { sequelize, Contract } = require('./model');
+const { sequelize, Contract, Profile } = require('./model');
 const { Op, ExclusionConstraintError } = require("sequelize");
 const { getProfile } = require('./middleware/getProfile');
 const httpStatus = require('http-status');
@@ -75,7 +75,7 @@ const getUnpaidJobs = async (req, res) => {
         limit: PAGINATION_LIMIT,
         offset: PAGINATION_LIMIT * page,
         where: { 
-            'paid': { [Op.not]: true } 
+            'paid': { [Op.not]: true }
         },
         include: [
             { 
@@ -96,6 +96,59 @@ const getUnpaidJobs = async (req, res) => {
     res.json(jobs);
 };
 
+const postPayJob = async (req, res) => {
+    const { job_id } = req.params;
+
+    const { Job, Contract } = req.app.get('models');
+    const { id: profile_id, type } = req.profile.dataValues;
+
+    if (type !== 'client') {
+        return res
+            .status(httpStatus.NOT_FOUND)
+            .send('Only clients can pay for jobs!')
+            .end();
+    }
+
+    let job = await Job.findOne({
+        where: {
+            'id': job_id,
+            'paid': { [Op.not]: true },
+        },
+        include: [
+            { 
+                model: Contract, 
+                where: {
+                    'ClientId': profile_id,
+                },
+                required: true,
+            },
+        ],
+    });
+
+    if (!job) {
+        return res
+            .status(httpStatus.NOT_FOUND)
+            .send('The job is not found or already paid.')
+            .end();
+    }
+
+    const getBalance = async (profileId) => (await Profile.findOne({ where: { id: profileId } })).balance;
+
+    if ((await getBalance(profile_id)) < job.price) {
+        return res.status(httpStatus.BAD_REQUEST).send('Insufficient balance!').end();
+    }
+
+    const result = await job.update({
+        paid: true,
+        paymentDate: new Date(),
+    });
+    // TODO: Update client and contractor balances
+    // TODO: Add transaction
+
+    return res.json(result);
+};
+
+
 const notImplemented = async (req, res) => {
     return res.status(httpStatus.NOT_IMPLEMENTED).end();
 };
@@ -106,7 +159,7 @@ const notImplemented = async (req, res) => {
 app.get('/contracts/:id', getProfile, getContractById);
 app.get('/contracts', getProfile, getContracts);
 app.get('/jobs/unpaid', getProfile, getUnpaidJobs);
-app.post('/jobs/:job_id/pay', getProfile, notImplemented);
+app.post('/jobs/:job_id/pay', getProfile, postPayJob);
 app.post('/balances/deposit/:userId', getProfile, notImplemented);
 app.post('/admin/best-profession?start=<date>&end=<date>', getProfile, notImplemented);
 app.get('/admin/best-clients?start=<date>&end=<date>&limit=<integer>', getProfile, notImplemented);
