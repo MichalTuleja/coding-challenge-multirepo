d9cac059c3a91747715421d4824598f3174eef4d
Michal Tuleja <michal.r.tuleja@gmail.com>
2023-01-25 04:09:18 +0100

    Update fallback profile ID

--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -3,7 +3,7 @@
  * That constant simulates the case when a user is authenticated
  * and has access to resources related them.
  */
-const FALLBACK_PROFILE_ID = 7;
+const FALLBACK_PROFILE_ID = 0;
 
 const getProfile = async (req, res, next) => {
     const { Profile } = req.app.get('models');
