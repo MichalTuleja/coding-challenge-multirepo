8d96392d8c0b7d22b2a0d6e8eae6adc1acb176c0
Michal Tuleja <michal.r.tuleja@gmail.com>
2023-02-20 00:24:10 +0100

    Add extra controllers and auth

--- nest-backend-ts/src/pokemons/pokemons.controller.ts
+++ nest-backend-ts/src/pokemons/pokemons.controller.ts
@@ -32,6 +32,15 @@ export class PokemonsController {
   async update(@Body() createPokemonDto: CreatePokemonDto) {
     this.pokemonsService.save(createPokemonDto);
   }
+  // @Put(':id')
+  // async updateById(@Param('id') id: number, @Body() updatePokemonDto: UpdatePokemonDto): Promise<string> {
+  //   // TODO: Validate id
+
+  //   // TODO: Update the entity
+
+  //   return `OK ${id}`;
+  // }
+  // TODO: Consider @Patch(':id')
 
   @Get()
   async findAll(): Promise<Pokemon[]> {
@@ -42,22 +51,6 @@ export class PokemonsController {
   findOne(@Param('id', new ParseIntPipe()) id: number) {
     return this.pokemonsService.findById(id);
   }
-
-  // @Get(':id')
-  // async getById(@Param('id') id: number): Promise<string> {
-  //   return `Hello ${id}`;
-  // }
-
-  // @Put(':id')
-  // async updateById(@Param('id') id: number, @Body() updatePokemonDto: UpdatePokemonDto): Promise<string> {
-  //   // TODO: Validate id
-
-  //   // TODO: Update the entity
-
-  //   return `OK ${id}`;
-  // }
-
-  // @Patch(':id')
 }
 
 @UseGuards(RolesGuard)
@@ -66,6 +59,7 @@ export class DamageController {
   constructor(private readonly pokemonsService: PokemonsService) {}
 
   @Get()
+  @Roles('admin')
   async getDamage(@Query() query): Promise<number> {
     // TODO: Validate id1 to match pokemon id
     // TODO: Validate id2 to match pokemon id
@@ -82,5 +76,49 @@ export class DamageController {
     const result = (30 * attacker.Attack / defender.Defense) * typeModifier
 
     return result;
+  }
+}
+
+@UseGuards(RolesGuard)
+@Controller('ringfight')
+export class RingFightController {
+  constructor(private readonly pokemonsService: PokemonsService) {}
+
+  @Get()
+  async getDamage(@Query() query): Promise<string> {
+    // TODO: Validate ids to match pokemon id
+
+    const ids = query.ids.split(',');
+
+    let ringFightWinner;
+    const pokemons = await Promise.all(ids.map(id => this.pokemonsService.findById(id)));
+
+    pokemons.map((p) => ({ ...p, defeated: false}));
+
+    // Be careful, that function changes order
+    const pickRandomTwo = (list: Pokemon[]): Pokemon[] => {
+      const shuffled = list.sort(() => 0.5 - Math.random());
+      return [shuffled[0], shuffled[1]];
+    }
+
+    const simulateFight = (p1: Pokemon, p2: Pokemon) => { // TODO: Add return type
+      return { winner: p1, loser: p2 };
+    }
+
+    // We pick a pokemon randomly each time
+    // Alternatively we could use a binary tree
+    for (let i = 0; i < pokemons.length**2; i++) { // Make sure the loop will finally end
+      const winningPokemons = pokemons.filter((p) => p.defeated == true);
+      if (winningPokemons.length == 1) {
+        ringFightWinner = winningPokemons[0];
+        break;
+      }
+
+      const [ pok1, pok2 ] = pickRandomTwo(pokemons.filter((p) => p.defeated == false));
+      const { winner, loser } = simulateFight(pok1, pok2);
+      // loser.defeated = true;
+    }
+
+    return ringFightWinner;
   }
 }
--- nest-backend-ts/src/pokemons/pokemons.module.ts
+++ nest-backend-ts/src/pokemons/pokemons.module.ts
@@ -3,9 +3,13 @@ import { PokemonsController, DamageController } from './pokemons.controller';
 import { PokemonsService } from './pokemons.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pokemon } from './pokemon.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pokemon])],
+  imports: [
+    TypeOrmModule.forFeature([Pokemon]),
+    AuthModule,
+  ],
   controllers: [
     PokemonsController,
     DamageController,
