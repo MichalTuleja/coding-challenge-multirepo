cc209bc5238a1c630bef1824d8d9526aaeefe676
Michal Tuleja <michal.r.tuleja@gmail.com>
2023-02-20 03:34:11 +0100

    Update typeModifier calculation

--- nest-backend-ts/src/pokemons/pokemons.service.ts
+++ nest-backend-ts/src/pokemons/pokemons.service.ts
@@ -26,16 +26,19 @@ export class PokemonsService {
     await this.pokeRepository.delete(id);
   }
 
+  determineTypeModifier(attacker: Pokemon, defender: Pokemon): number {
+    if(attacker['Type 1'] === 'electric' || attacker['Type 2'] === 'electric') {
+      if (defender['Type 1'] === 'water' || defender['Type 2'] === 'water') return 2;
+      if (defender['Type 1'] === 'rock' || defender['Type 2'] === 'rock') return 0.5;
+    }
+    return 1;
+  }
+
   async calculateDamage(attackerId: number, defenderId: number): Promise<number> {
     const attacker = await this.findById(attackerId);
     const defender = await this.findById(defenderId);
 
-    // TODO: Update the typeModifier logic to support the following logic:
-    // - When the attacker type is electric and the defender is water it should return 2
-    // - When the attacker type is electric and the defender is rock it should return 0.5
-    // - Any other scenario should return 1
-    const typeModifier = 1;
-
+    const typeModifier = this.determineTypeModifier(attacker, defender);
     const result = (30 * attacker.Attack / defender.Defense) * typeModifier;
 
     return result;
